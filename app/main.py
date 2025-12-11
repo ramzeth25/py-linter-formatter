e14f8ba69956741767c36eefa62a6ba69dbac07e
ramzeth <ramzeth25@gmail.com>
2025-12-11 16:33:29 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -7,6 +7,7 @@ def format_linter_error(error: dict) -> dict:
         "source": "flake8"
     }
 
+
 def format_single_linter_file(file_path: str, errors: list) -> dict:
     return {
         "errors": [
@@ -23,5 +24,7 @@ def format_linter_report(linter_report: dict) -> list:
         format_single_linter_file(path, errors)
         for path, errors in linter_report.items()
     ]
+
+
 
 
